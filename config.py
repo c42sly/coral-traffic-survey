2644c306142c67d3574c5f6ce6752fb38bd69699
c42sly <c42sly@hotmail.com>
2026-07-04 05:08:51 +0100

    Clarify TRACKER_MAX_DISTANCE in config.py
    Added comment to clarify TRACKER_MAX_DISTANCE value.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ MODEL_CLASSIFIER = 'traffic_classifier_quant_edgetpu.tflite'
 LABELS_FILE = 'labels.txt'
 VIDEO_DEVICE = 'rtsp://admin:fordsyke1944@192.168.1.119:554/ch0_0.264#media=video'
 
-TRACKER_MAX_DISTANCE = 500
+TRACKER_MAX_DISTANCE = 500   #####Pixels
 TRACKER_MAX_FRAMES = 120
 TRACKER_MAX_MISSING = 60
 CROP_PADDING_RATIO = 0.35  # The 35% margin boost
